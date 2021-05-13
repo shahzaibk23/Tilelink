a4f2158b51ad833c18eee16efcaa55a4eef41d15
shahzaib <shahzaibceo@gmail.com>
2021-05-13 17:57:22 +0500

    Channel D Bundle added

--- src/main/scala/tilelink/top.scala
+++ src/main/scala/tilelink/top.scala
@@ -11,14 +11,27 @@ trait Config {
     val i = 32
 }
 
-val channelABundle extends Bundle with Config {
-    val a_opcode = Input(UInt(3.W))
-    val a_param = Input(UInt(3.W))
-    val a_size = Input(UInt(z.W))
-    val a_source = Input(UInt(o.W))
-    val a_address = Input(UInt(a.W))
-    val a_mask = Input(UInt(w.W))
-    val a_corrupt = Input(UInt(1.W))
-    val a_data = Input(UInt((8*w).W))
+class channelABundle extends Bundle with Config {
+    val a_opcode = UInt(3.W)
+    val a_param = UInt(3.W)
+    val a_size = UInt(z.W)
+    val a_source = UInt(o.W)
+    val a_address = UInt(a.W)
+    val a_mask = UInt(w.W)
+    val a_corrupt = UInt(1.W)
+    val a_data = UInt((8*w).W)
 }
+
+class channelDBundle extends Bundle {
+    val d_opcode = UInt(3.W)
+    val d_param = UInt(2.W)
+    val d_size = UInt(z.W)
+    val d_source = UInt(o.W)
+    val d_sink = UInt(i.W)
+    val d_denied = UInt(1.W)
+    val d_corrupt = UInt(1.W)
+    val d_data = UInt((8*w).W)
+}
+
+
 
