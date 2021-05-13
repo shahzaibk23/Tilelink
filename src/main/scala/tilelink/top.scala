a6e91dd07870e26e2399e6c729246f92e9926f77
shahzaib <shahzaibceo@gmail.com>
2021-05-13 16:35:40 +0500

    Channel A Bundle added

--- src/main/scala/tilelink/top.scala
+++ src/main/scala/tilelink/top.scala
@@ -10,3 +10,15 @@ trait Config {
     val o = 32
     val i = 32
 }
+
+val channelABundle extends Bundle with Config {
+    val a_opcode = Input(UInt(3.W))
+    val a_param = Input(UInt(3.W))
+    val a_size = Input(UInt(z.W))
+    val a_source = Input(UInt(o.W))
+    val a_address = Input(UInt(a.W))
+    val a_mask = Input(UInt(w.W))
+    val a_corrupt = Input(UInt(1.W))
+    val a_data = Input(UInt((8*w).W))
+}
+
