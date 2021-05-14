2ee9282c3f07047237bfb844ae7756c8af7c6bcc
shahzaib <shahzaibceo@gmail.com>
2021-05-14 22:59:23 +0500

    changes made

--- src/main/scala/tilelink/top.scala
+++ src/main/scala/tilelink/top.scala
@@ -2,6 +2,7 @@ package tilelink
 
 import chisel3._ 
 import chisel3.util._ 
+import chisel3.experimental.BundleLiterals._
 
 trait Config {
     val w = 4
@@ -13,7 +14,7 @@ trait Config {
 
 trait OpCodes {
     val Get = 4
-    val AccessAckData = 1
+    val AccessAckData = 1.U
     val PutFullData = 0
     val PutPartialData = 1
     val AccessAck = 0
@@ -41,7 +42,7 @@ class channelDBundle extends Bundle with Config {
     val d_data = UInt((8*w).W)
 }
 
-class Top extends Module with OpCodes {
+class Top extends Module with OpCodes with Config {
 
     val io = IO(new Bundle{
         // val opCode = Input(UInt(3.W))
@@ -49,24 +50,79 @@ class Top extends Module with OpCodes {
         val channelD = Decoupled(new channelDBundle)
     })
 
+    io.channelA.ready := 1.B
+
     val memory = SyncReadMem(1024,UInt(32.W))
-    val r = Reg(UInt(32.W))
+    
 
     memory.write(2.U, 4.U)
 
+    val reg_opcode = Reg(UInt(3.W))
+    val reg_param = Reg(UInt(2.W))
+    val reg_size = Reg(UInt(z.W))
+    val reg_source = Reg(UInt(o.W))
+    val reg_sink = Reg(UInt(i.W))
+    val reg_denied = Reg(UInt(1.W))
+    val reg_corrupt = Reg(UInt(1.W))
+    val reg_data = Reg(UInt(8.W))
+    val reg_valid = Reg(Bool())
 
-    io.channelA.ready := 1.B
+    reg_opcode := 2.U
+    reg_param := 3.U
+    reg_size := io.channelA.bits.a_size
+    reg_source := io.channelA.bits.a_source
+    reg_sink := 6.U
+    reg_denied := 1.U
+    reg_corrupt := 1.U //io.channelA.bits.a_corrupt
+    reg_data := memory.read(io.channelA.bits.a_address)
+    reg_valid := io.channelA.valid
 
-    when(io.channelA.bits.a_opcode === Get.U){
-        io.channelD.bits.d_opcode := io.channelA.bits.a_opcode
-        io.channelD.bits.d_param := io.channelA.bits.a_param
-        io.channelD.bits.d_size := io.channelA.bits.a_size
-        io.channelD.bits.d_source := io.channelA.bits.a_source
-        io.channelD.bits.d_sink := 0.U
-        io.channelD.bits.d_denied := 0.U
-        io.channelD.bits.d_corrupt := io.channelA.bits.a_corrupt
+    // val r = RegInit({
+    //     val bundle = Wire(new channelDBundle)
+    //     bundle.d_opcode := AccessAckData.U
+    //     bundle.d_param := 0.U
+    //     bundle.d_size := io.channelA.bits.a_size
+    //     bundle.d_source := io.channelA.bits.a_source
+    //     bundle.d_sink := 0.U
+    //     bundle.d_denied := 0.U 
+    //     bundle.d_corrupt := io.channelA.bits.a_corrupt
+    //     bundle.d_data := memory.read(io.channelA.bits.a_address)
+    //     bundle
+    // })
+
+    // val reg = RegInit((new channelDBundle).Lit(
+    //     _.d_opcode -> AccessAckData.U,
+    //     _.d_param -> 0.U,
+    //     _.d_size -> io.channelA.bits.a_size,
+    //     _.d_source -> io.channelA.bits.a_source,
+    //     _.d_sink -> 0.U,
+    //     _.d_denied -> 0.U, 
+    //     _.d_corrupt -> io.channelA.bits.a_corrupt,
+    //     _.d_data -> memory.read(io.channelA.bits.a_address)
+    // ))
+
+
+    
+
+    when(io.channelA.bits.a_opcode === Get.U ){
+        // io.channelD.bits.d_opcode := AccessAckData.U
+        // io.channelD.bits.d_param := 0.U
+        // io.channelD.bits.d_size := io.channelA.bits.a_size
+        // io.channelD.bits.d_source := io.channelA.bits.a_source
+        // io.channelD.bits.d_sink := 0.U
+        // io.channelD.bits.d_denied := 0.U
+        // io.channelD.bits.d_corrupt := io.channelA.bits.a_corrupt
+        // io.channelD.bits.d_data := memory.read(io.channelA.bits.a_address)
+        // io.channelD.bits := r
+        io.channelD.bits.d_opcode := reg_opcode
+        io.channelD.bits.d_param := reg_param
+        io.channelD.bits.d_size := reg_size
+        io.channelD.bits.d_source := reg_source
+        io.channelD.bits.d_sink := reg_sink
+        io.channelD.bits.d_denied := reg_denied
+        io.channelD.bits.d_corrupt := reg_corrupt
         io.channelD.bits.d_data := memory.read(io.channelA.bits.a_address)
-        io.channelD.valid := io.channelA.valid
+        io.channelD.valid := reg_valid
     }.otherwise{
         io.channelD.bits.d_opcode := io.channelA.bits.a_opcode
         io.channelD.bits.d_param := io.channelA.bits.a_param
