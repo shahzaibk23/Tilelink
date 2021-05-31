16c897309ce719f16df504acd89ebb7e702106cb
shahzaib <shahzaibceo@gmail.com>
2021-06-01 04:12:16 +0500

    Channels Details updated

--- src/main/scala/tilelink/channelA.scala
+++ src/main/scala/tilelink/channelA.scala
@@ -10,6 +10,18 @@ class channelABundle extends Bundle with Config {
     val a_source = UInt(o.W)
     val a_address = UInt(a.W)
     val a_mask = UInt(w.W)
-    val a_corrupt = UInt(1.W)
+    val a_corrupt = Bool()
     val a_data = UInt((8*w).W)
 }
+
+// Channel A pin details
+
+//          Signal Name  | No of Bits           | Description                                                                                         
+//          a_opcode     | [2:0]                | Request opcode (read, write, or partial write)                                                      
+//          a_param      | [2:0]                | Unused/Ignored                                                                                      
+//          a_size       | config.z [2:0]       | Request size (requested size is 2^a_size, thus 0 = byte, 1 = 16b, 2 = 32b, 3 = 64b, etc)            
+//          a_source     | config.o [8:0]       | Request identifier of configurable width                                                            
+//          a_address    | config.a [32:0]      | Request address of configurable width                                                               
+//          a_mask       | config.w [4:0]       | Write strobe, one bit per byte indicating which lanes of data are valid for this write request      
+//          a_corrupt    | Bool                 |  Reserved; must be 0                                                                                
+//          a_data       | config.w x 8 [32:0]  | Write request data of configurable width                                                            
--- src/main/scala/tilelink/channelD.scala
+++ src/main/scala/tilelink/channelD.scala
@@ -9,7 +9,19 @@ class channelDBundle extends Bundle with Config {
     val d_size = UInt(z.W)
     val d_source = UInt(o.W)
     val d_sink = UInt(i.W)  
-    val d_denied = UInt(1.W)
-    val d_corrupt = UInt(1.W)
+    val d_denied = Bool()
+    val d_corrupt = Bool()
     val d_data = UInt((8*w).W)
 }
+
+// Channel D pin details
+
+//          Signal Name  | No of Bits           | Description                                                                                         
+//          d_opcode     | [2:0]                | Response opcode (Ack or Data)                                                                       
+//          d_param      | [2:0]                | Response parameter (unused)                                                                         
+//          d_size       | config.z [2:0]       | Response data size                                                                                  
+//          d_source     | config.o [8:0]       | Bouncing of request ID of configurable width                                                        
+//          d_sink       | config.i [0]         | Response ID of configurable width (possibly unused)                                                 
+//          d_denied     | Bool                 | The slave was unable to service the request.                                                        
+//          d_corrupt    | Bool                 | Reserved; must be 0                                                                                 
+//          d_data       | config.w x 8 [32:0]  | Response data of configurable width                                                                 
