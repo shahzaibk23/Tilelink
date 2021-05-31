c73374a81cd5e8fe5a7f681e2e3737e550365b25
shahzaib <shahzaibceo@gmail.com>
2021-06-01 04:11:54 +0500

    Config details updated

--- src/main/scala/tilelink/config.scala
+++ src/main/scala/tilelink/config.scala
@@ -5,9 +5,19 @@ import chisel3.util._
 
 
 trait Config {
-    val w = 4
-    val a = 32
-    val z = 8
-    val o = 32
-    val i = 32
+    val w: Int = 4
+    val a: Int = 32
+    val z: Int = 2
+    val o: Int = 8
+    val i: Int = 1
 }
+
+    /*
+    
+    w => Width of the data bus in bytes.                                  || DUW => width of device user bits, default 4
+    a => Width of each address field in bits.                             || AW => width of address bus, default 32
+    z => Width of each size field in bits.                                || SZW => size width, covers 2^(x) <= DBW; (2 bit for 4B)
+    o => Number of bits needed to disambiguate per-link master sources.   || AIW => width of address source (ID) bus, default 8
+    i => Number of bits needed to disambiguate per-link slave sinks.      || DIW => width of sink bits, default 1
+
+    */
